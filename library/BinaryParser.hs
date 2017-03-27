fd5f88f983d44244b0f53681c6c9eb664b217b01
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-03-27 16:48:48 +0300

    Extras

--- library/BinaryParser.hs
+++ library/BinaryParser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module BinaryParser
 (
   BinaryParser,
@@ -10,6 +11,14 @@ module BinaryParser
   remainders,
   endOfInput,
   sized,
+  -- * Extras
+  storableOfSize,
+  beWord16,
+  leWord16,
+  beWord32,
+  leWord32,
+  beWord64,
+  leWord64,
 )
 where
 
@@ -17,6 +26,7 @@ import BinaryParser.Prelude
 import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Unsafe as ByteString
 import qualified Success.Pure as Success
+import qualified Data.ByteString.Internal as A
 
 
 -- |
@@ -112,3 +122,81 @@ sized size (BinaryParser stateT) =
         evalStateT stateT (ByteString.unsafeTake size remainders) &
         fmap (\result -> (result, ByteString.unsafeDrop size remainders))
       else Success.failure "End of input"
+
+-- |
+-- Storable value of the given amount of bytes.
+{-# INLINE storableOfSize #-}
+storableOfSize :: Storable a => Int -> BinaryParser a
+storableOfSize size =
+  BinaryParser $ StateT $ \(A.PS payloadFP offset length) ->
+    if length >= size
+      then let result = unsafeDupablePerformIO $ withForeignPtr payloadFP $ \ptr -> peek (castPtr ptr)
+               newRemainder = A.PS payloadFP (offset + size) (length - size)
+               in return (result, newRemainder)
+      else Success.failure "End of input" 
+
+-- | Big-endian word of 2 bytes.
+{-# INLINE beWord16 #-}
+beWord16 :: BinaryParser Word16
+#ifdef WORDS_BIGENDIAN
+beWord16 =
+  storableOfSize 2
+#else
+beWord16 =
+  byteSwap16 <$> storableOfSize 2
+#endif
+
+-- | Little-endian word of 2 bytes.
+{-# INLINE leWord16 #-}
+leWord16 :: BinaryParser Word16
+#ifdef WORDS_BIGENDIAN
+leWord16 =
+  byteSwap16 <$> storableOfSize 2
+#else
+leWord16 =
+  storableOfSize 2
+#endif
+
+-- | Big-endian word of 4 bytes.
+{-# INLINE beWord32 #-}
+beWord32 :: BinaryParser Word32
+#ifdef WORDS_BIGENDIAN
+beWord32 =
+  storableOfSize 4
+#else
+beWord32 =
+  byteSwap32 <$> storableOfSize 4
+#endif
+
+-- | Little-endian word of 4 bytes.
+{-# INLINE leWord32 #-}
+leWord32 :: BinaryParser Word32
+#ifdef WORDS_BIGENDIAN
+leWord32 =
+  byteSwap32 <$> storableOfSize 4
+#else
+leWord32 =
+  storableOfSize 4
+#endif
+
+-- | Big-endian word of 8 bytes.
+{-# INLINE beWord64 #-}
+beWord64 :: BinaryParser Word64
+#ifdef WORDS_BIGENDIAN
+beWord64 =
+  storableOfSize 8
+#else
+beWord64 =
+  byteSwap64 <$> storableOfSize 8
+#endif
+
+-- | Little-endian word of 8 bytes.
+{-# INLINE leWord64 #-}
+leWord64 :: BinaryParser Word64
+#ifdef WORDS_BIGENDIAN
+leWord64 =
+  byteSwap64 <$> storableOfSize 8
+#else
+leWord64 =
+  storableOfSize 8
+#endif
