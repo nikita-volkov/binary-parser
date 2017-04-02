7a7d1495349d44945e4ca81989c2ff4e806d70d2
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-04-02 22:50:46 +0300

    Spot a bug in composition

--- tests/Main.hs
+++ tests/Main.hs
@@ -20,12 +20,30 @@ main =
     builderIsomporhismProperty "beWord16" B.beWord16 C.word16BE
     ,
     builderIsomporhismProperty "beWord32" B.beWord32 C.word32BE
+    ,
+    expectedResultTest "byte consumes" ((,) <$> B.byte <*> B.beWord32) (49, 4) "1\NUL\NUL\NUL\EOT"
+    ,
+    expectedResultTest "Applicative composition" ((,) <$> B.beWord16 <*> B.beWord16) (1, 2) "\NUL\SOH\NUL\STX"
+    ,
+    let parser =
+          do
+            a <- B.beWord16
+            b <- B.beWord16
+            return (a, b)
+        in expectedResultTest "Monadic composition" parser (1, 2) "\NUL\SOH\NUL\STX"
   ]
 
-builderIsomporhismProperty subjectName parser valueToBuilder =
+builderIsomporhismProperty details parser valueToBuilder =
   testProperty name $ \value ->
   B.run parser (D.toStrict (C.toLazyByteString (valueToBuilder value))) ===
   Right value
   where
     name =
-      "Builder isomorphism: " <> subjectName
+      "builderIsomporhismProperty: " <> details
+
+expectedResultTest details parser expectedValue input =
+  testCase name $ do
+    assertEqual "" (Right expectedValue) (B.run parser input)
+  where
+    name =
+      "expectedResultTest: " <> details
